3260f249837c669b2bb896ad336a684823b794b5
DominiqueOyco <dominickoyco@gmail.com>
2020-12-19 21:32:42 -0800

    ...

---
+++ BarkyDog/Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
