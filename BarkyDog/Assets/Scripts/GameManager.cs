0b2aa6d818d00d5679af297af67067b1028ed0ef
DominiqueOyco <dominickoyco@gmail.com>
2020-12-26 11:51:18 -0800

    ...

--- BarkyDog/Assets/Scripts/GameManager.cs
+++ BarkyDog/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -17,5 +18,10 @@ public class GameManager : MonoBehaviour
     {
         gameOverCanvas.SetActive(true);
         Time.timeScale = 0;
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
     }
 }
